1b523290e22d61d1254b4864b05176c53bb17d3b
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-07-12 13:18:10 -0400

    Updated: Indent guide; Added: NPM task runner, ESLint config

--- process.php
+++ process.php
@@ -14,7 +14,7 @@ $mail->SMTPDebug = 3;
 $mail->setFrom('noreply@dirnovo.com', 'Clicnovo Mailer');
 $mail->Sender='novotecn@gator3249.hostgator.com';
 $mail->addAddress('example@example.com'); // Esta línea, corresponde al mail del cliente
-$mail->addBCC('mario@clicnovo.com');
+$mail->addBCC('formularios@clicnovo.com');
 $mail->addReplyTo($email, $nombre);
 $mail->Subject = "Formulario: " . $asunto;
 $mail->isHTML(true);
