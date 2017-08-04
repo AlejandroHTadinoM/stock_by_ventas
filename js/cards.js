bf6671003e691301a5bfd811bb02104e01102a7f
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-08-04 15:49:44 -0400

    Q&A

---
+++ js/cards.js
@@ -0,0 +1,10 @@
+function resizeCards () {
+	var heights = $(".size").map(function() {		        
+			return $(this).height();		   
+		}).get(),
+	maxHeight = Math.max.apply(null, heights);
+	$(".card").height(maxHeight);
+}
+
+$(window).resize(resizeCards());
+resizeCards();
